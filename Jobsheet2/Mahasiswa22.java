0871fdc7be2500587adf50ba1ab1f0f3a2475bbf
rafaachyana <rafaachyana778@gmail.com>
2026-02-20 08:35:10 +0700

    first commit

---
+++ Jobsheet2/Mahasiswa22.java
@@ -0,0 +1,34 @@
+package Jobsheet2;
+public class Mahasiswa22 {
+    
+}
+class Mahasiswa {
+    String nama;
+    String nim;
+    String kelas;
+    double ipk;
+
+    void tampilInformasi(){
+        System.out.println("Nama; " + nama);
+        System.out.println("NIM; " + nim);
+        System.out.println("IPK; " + ipk);
+        System.out.println("Kelas; " + kelas);
+    }
+    void ubahKelas(String kelasBaru){
+        kelas = kelasBaru;
+    }
+    void updateIpk(double ipkBaru){
+        ipk = ipkBaru;
+    }
+    String nilaiKinerja(){
+        if (ipk >= 3.5) {
+            return "Sangat Baik";
+        } else if (ipk >= 3.0) {
+            return "Baik";
+        } else if (ipk >= 2.5) {
+            return "Cukup";
+        } else {
+            return "Kurang";
+        }
+    }
+}
