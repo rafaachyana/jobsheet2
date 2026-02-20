59014a0a8ef12f4d9342bfaa32a227fb2479d76b
rafaachyana <rafaachyana778@gmail.com>
2026-02-20 09:41:30 +0700

    hehe

--- Jobsheet2/Mahasiswa22.java
+++ Jobsheet2/Mahasiswa22.java
@@ -18,7 +18,11 @@ class Mahasiswa {
         kelas = kelasBaru;
     }
     void updateIpk(double ipkBaru){
+    if (ipkBaru >= 0.0 && ipkBaru <= 4.0) {
         ipk = ipkBaru;
+    } else {
+        System.out.println("IPK tidak valid. Harus antara 0.0 dan 4.0");
+    }
     }
     String nilaiKinerja(){
         if (ipk >= 3.5) {
