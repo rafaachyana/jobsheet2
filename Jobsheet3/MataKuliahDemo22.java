b254df12de8750bfb9bffc0219204f8f34896a35
rafaachyana <rafaachyana778@gmail.com>
2026-02-27 10:04:39 +0700

    modif

--- Jobsheet3/MataKuliahDemo22.java
+++ Jobsheet3/MataKuliahDemo22.java
@@ -22,6 +22,15 @@ public class MataKuliahDemo22 {
             System.out.println("---------------------------------------");
             arrayOfMataKuliah[i]=new MataKuliah22(kode, nama, sks, jumlahJam);
         }
+        
+        for (int i=0; i<3; i++){
+            System.out.println("Data Matakuliah ke-"+(i+1));
+            System.out.println("Kode     : "+arrayOfMataKuliah[i].kode);
+            System.out.println("Nama     : "+arrayOfMataKuliah[i].nama);
+            System.out.println("SKS      : "+arrayOfMataKuliah[i].sks);
+            System.out.println("Jumlah Jam : "+arrayOfMataKuliah[i].jumlahJam);
+            System.out.println("---------------------------------------");
+        }
         sc.close();
     } 
 }
