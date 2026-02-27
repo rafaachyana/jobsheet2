5791bd499ffa5541ae95c1de8a7eadd8daeb15dd
rafaachyana <rafaachyana778@gmail.com>
2026-02-27 09:35:37 +0700

    modif2(cetakingfo)

--- Jobsheet3/Mahasiswa22.java
+++ Jobsheet3/Mahasiswa22.java
@@ -4,5 +4,12 @@ public class Mahasiswa22 {
     public String nama;
     public String kelas;
     public float ipk;
+
+    void cetakInfo(){
+        System.out.println("NIM : "+nim);
+        System.out.println("Nama : "+nama);
+        System.out.println("Kelas : "+kelas);
+        System.out.println("IPK : "+ipk);
+    }
 }
 
--- Jobsheet3/MahasiswaDemo22.java
+++ Jobsheet3/MahasiswaDemo22.java
@@ -23,11 +23,8 @@ public class MahasiswaDemo22 {
 
         for(int i=0; i<3; i++){
             System.out.println("Data Mahasiswa ke-"+(i+1));
-            System.out.println("NIM    : "+arrayOfMahasiswa[i].nim);
-            System.out.println("Nama : "+arrayOfMahasiswa[i].nama);
-            System.out.println("Kelas : "+arrayOfMahasiswa[i].kelas);
-            System.out.println("IPK  : "+arrayOfMahasiswa[i].ipk);
-            System.out.println("--------------------------------------------");
+            arrayOfMahasiswa[i].cetakInfo();
+            System.out.println("---------------------------------------");
         }
     }
 }
